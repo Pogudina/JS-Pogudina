e2aa7f2e0f2a7775189372118ae900c33e60af8a
Pogudina <80483287+Pogudina@users.noreply.github.com>
2023-01-18 12:46:04 +0500

    Commit first lab js

---
+++ 1/index.php
@@ -0,0 +1,18 @@
+<html>
+<head> <title> Лаба1 </title> </head>
+<div align="left">
+  <a  href="http://a0765723.xsph.ru/index.php">Вернуться на главную </a>  
+</div>
+<body align="center">
+<h1>Лабораторная работа №1 - Введение в JavaScript</h1><p>
+<h2>Погудина М.К. ПИ-423</h2><p>
+<u1>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab1.html">Задание 1. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab2.html">Задание 2. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab3.html">Задание 3. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab4.html">Задание 4. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab5.html">Задание 5. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab6.html">Задание 6. </a></li>
+        <li><a href="http://a0765723.xsph.ru/js/1/Lab7.html">Задание 7. </a></li>
+</u1>
+</html>
